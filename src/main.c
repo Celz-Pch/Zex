a9f15926f4869a9a739e36c6238947998c4e0844
czp <celenzo.peuch@epitech.eu>
2026-02-18 22:09:31 +0100

    feat(main): main loop fix

--- src/main.c
+++ src/main.c
@@ -1,5 +1,5 @@
 /*
-** EPITECH PROJECT, 2026
+** Cz PROJECT, 2026
 ** main
 ** File description:
 ** main
@@ -7,15 +7,39 @@
 
 #include "zex.h"
 
-int main(int argc, char **argv)
+static void setup_ncurses(void)
 {
-    if (argc != 2)
-        return 84;
     initscr();
     raw();
     keypad(stdscr, TRUE);
     noecho();
-    main_loop();
+    start_color();
+    init_colors();
+}
+
+static editor_t *open_editor(char *path)
+{
+    struct stat st;
+
+    if (stat(path, &st) == 0 && S_ISDIR(st.st_mode))
+        return init_editor(NULL, path);
+    return init_editor(path, NULL);
+}
+
+int main(int argc, char **argv)
+{
+    editor_t *ed;
+
+    if (argc != 2)
+        return 84;
+    setup_ncurses();
+    ed = open_editor(argv[1]);
+    if (!ed) {
+        endwin();
+        return 84;
+    }
+    main_loop(ed);
+    free_editor(ed);
     endwin();
     return 0;
 }
